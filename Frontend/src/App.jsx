8319e9428896f897797be7fbdf784bc507978648
Sahil Rajput <sahilrazput18@gmail.com>
2025-06-16 22:42:25 +0530

    debug

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,14 +3,13 @@ import { Routes, Route } from "react-router-dom";
 import UserProtectedWrapper from "./protectedWrapper/UserProtectedWrapper";
 import axios from "axios";
 import Signup from "./pages/Signup";
-const VideoComponent = lazy(() => import("./pages/VideoComponent"));
 const Call = lazy(() => import("./pages/Call"));
 const PageNotFound = lazy(() => import("./pages/PageNotFound"));
 const Home = lazy(() => import("./pages/Home"));
 const Profile = lazy(() => import("./pages/Profile"));
 const Login = lazy(() => import("./pages/Login"));
 const Landing = lazy(() => import("./pages/LandingPage"));
-const Payment = lazy(() => import("./pages/Payment"));
+// const VideoComponent = lazy(() => import("./pages/VideoComponent"));
 
 const App = () => {
   useEffect(() => {
@@ -41,7 +40,6 @@ const App = () => {
         }
       />
       <Route path="/signup" element={<Signup />} />
-      {/* <Route path="/payment" element={<Payment />} /> */}
       <Route path="/" element={<Landing />} />
       <Route path="/login" element={<Login />} />
       <Route path="/call" element={<Call />} />
